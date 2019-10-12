a06b0f020abafaae09408b5bb9a82b337cec12a4
trnny <thuawu27@gmail.com>
2019-10-12 10:09:47 +0800

    手动添加了.sh文件,方便调试,学会怎么vscode一键编译多文件后再说吧

--- main.cpp
+++ main.cpp
@@ -8,3 +8,7 @@ int main(){
     cout << (string)a << '\n' << b << '\n' << c << '\n' << d << endl;
     return 0;
 }
+
+//  编译指令    放弃了 不知道在linux下多文件怎么自动汇编 
+//  百度了要用到make,麻烦死了,主要是不想目录下那么多杂七杂八的文件
+//  g++ -g /home/trnny/Documents/VSCode/C++/ds_bignum/main.cpp /home/trnny/Documents/VSCode/C++/ds_bignum/bignum.cpp -std=c++11 -o /home/trnny/Documents/VSCode/C++/ds_bignum/main
---
+++ run_linux.sh
@@ -0,0 +1,2 @@
+g++ -g /home/trnny/Documents/VSCode/C++/ds_bignum/main.cpp /home/trnny/Documents/VSCode/C++/ds_bignum/bignum.cpp -std=c++11 -o /home/trnny/Documents/VSCode/C++/ds_bignum/main
+./main
